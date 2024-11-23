b8c63bf393c8b9cf72d9e1579b653ed6344d1e2a
Akash Gupta <akash.g6787@gmail.com>
2024-11-23 11:42:07 +0530

    set-up-main

---
+++ backend/main.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+import json
+
+app = Flask(__name__)
+
+@app.route('/processing', method = 'POST')
+def data_processing():
+    if not request.is_json:
+        return jsonify({"No data from the backend"}), 400
+    
+    json_data = request.get_json
+
+    try:
+        return None
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+if __name__ == "__main__":
+    app.run(port = 5000, debug = True)
